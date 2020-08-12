6512908a2b5e5907d59171bf19284b3ff1ed7ebb
gjrjr4545 <gjrjr4545@gmail.com>
2020-08-12 21:21:13 +0900

    쓰레드 100개 테스트

--- README.md
+++ README.md
@@ -11,3 +11,14 @@
 ### 2. 본격적으로 들어가보자
        url : https://github.com/reactive-streams/reactive-streams-jvm
        
+       
+### Servlet 3.0 : 비동기 서블릿
+       - HTTP connection은 이미 논블록킹 IO
+       - 서블릿 요청 읽기, 응답쓰기는 블록킹
+       - 비동기 작업 시작 즉시 서블릿 쓰레드 반납
+       - 비동기 작업이 완료되면 서블릿 쓰레드 재할당
+       - 비동기 서블릿 컨텍스트 이용 (AsyncContext)
+### Servlet 3.1 : 논블록킹 IO
+        - 논블록킹 서블릿 요청, 응답 처리
+        - Callback
+       
--- src/main/java/com/reactive/streams/futurefx/DemoApplication.java
+++ src/main/java/com/reactive/streams/futurefx/DemoApplication.java
@@ -1,20 +1,13 @@
 package com.reactive.streams.futurefx;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.stereotype.Component;
-import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.concurrent.Callable;
 
-import java.util.concurrent.Future;
 
 /**
  * 토비의 봄 TV 8화 - 스프링 리엑티브 웹 개발 4부, 자바와 스프링의 비동기 개발 기술 살펴보기.
@@ -24,44 +17,36 @@ import java.util.concurrent.Future;
 @EnableAsync
 public class DemoApplication {
 
-	@Component
-	public static class MyService{
-		@Async(value="tp")
-		public ListenableFuture<String> hello() throws InterruptedException {
-			log.debug("hello()");
-			Thread.sleep(1000);
-			return new AsyncResult<>("hello");
+	@RestController
+	public static class MyController{
+
+		@GetMapping("/callable")
+		public Callable<String> callable() throws InterruptedException{
+
+			log.info(("async"));
+			return () -> {
+				log.info("");
+				Thread.sleep(2000);
+				return "hello";
+
+			};
 		}
+
+		@GetMapping("/callable2")
+		public String callable2() throws  InterruptedException{
+			log.info(("async"));
+			Thread.sleep(2000);
+			return "hello";
+		}
+
 	}
 
-	@Bean
-	ThreadPoolTaskExecutor tp() {
-		ThreadPoolTaskExecutor te = new ThreadPoolTaskExecutor();
-		te.setCorePoolSize(10); // 기본 Thread 수 (1)
-		te.setMaxPoolSize(100); // 최대 Thread 수 -> QUERE가 넘치는 경우 실행
-		te.setQueueCapacity(200); // QUEUE 수(2)
-		te.setThreadNamePrefix("myThread");
-		te.initialize();
-		return te;
-	}
 
 	public static void main(String[] args) {
-		try (ConfigurableApplicationContext c =  SpringApplication.run(DemoApplication.class, args)) {
-
-		};
+		 SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Autowired MyService myService;
 
-	@Bean
-	ApplicationRunner run(){
-		return args -> {
-			log.info("run()");
-			Future<String> f = myService.hello();
-			log.info("exit : " + f.isDone());
-			log.info("result : " + f.get());
 
-		};
-	}
 
 }
